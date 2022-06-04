01e683ac4a4045e2f0364bcfb455fef963147f67
FacelessTiger <thefacelesstiger@yahoo.com>
2022-06-04 08:01:38 -0700

    imgui events

--- Ellis/src/Ellis/ImGui/ImGuiLayer.h
+++ Ellis/src/Ellis/ImGui/ImGuiLayer.h
@@ -3,6 +3,10 @@
 #include "Ellis/Core.h"
 #include "Ellis/Layer.h"
 
+#include "Ellis/Events/ApplicationEvent.h"
+#include "Ellis/Events/KeyEvent.h"
+#include "Ellis/Events/MouseEvent.h"
+
 namespace Ellis {
 
 	class ELLIS_API ImGuiLayer : public Layer
@@ -17,6 +21,15 @@ namespace Ellis {
 		void OnDetach() override;
 		void OnUpdate() override;
 		void OnEvent(Event& event) override;
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
 	};
 
 }
