2fa40e8bbe91a8665b7fc964149f8a18f9e728b2
FacelessTiger <thefacelesstiger@yahoo.com>
2022-06-06 08:55:13 -0700

    Vertex buffer layout

--- Ellis/src/Platform/OpenGL/OpenGLBuffer.h
+++ Ellis/src/Platform/OpenGL/OpenGLBuffer.h
@@ -8,12 +8,16 @@ namespace Ellis {
 	{
 	private:
 		uint32_t m_RendererID;
+		BufferLayout m_Layout;
 	public:
 		OpenGLVertexBuffer(float* vertices, uint32_t size);
 		virtual ~OpenGLVertexBuffer();
 
 		void Bind() const override;
 		void Unbind() const override;
+
+		void SetLayout(const BufferLayout& layout) override { m_Layout = layout; }
+		const BufferLayout& GetLayout() const override { return m_Layout; }
 	};
 
 	class OpenGLIndexBuffer : public IndexBuffer
