fa9952540e18fd501a1e25356638a5c0a3ee8b54
FacelessTiger <thefacelesstiger@yahoo.com>
2022-06-10 10:21:57 -0700

    Renderer2D combining to one shader

--- Ellis/src/Platform/OpenGL/OpenGLTexture.h
+++ Ellis/src/Platform/OpenGL/OpenGLTexture.h
@@ -2,6 +2,8 @@
 
 #include "Ellis/Renderer/Texture.h"
 
+#include <glad/glad.h>
+
 namespace Ellis {
 
 	class OpenGLTexture2D : public Texture2D
@@ -10,12 +12,17 @@ namespace Ellis {
 		std::string m_Path;
 		uint32_t m_Width, m_Height;
 		uint32_t m_RendererID;
+		
+		GLenum m_InternalFormat, m_DataFormat;
 	public:
+		OpenGLTexture2D(uint32_t width, uint32_t height);
 		OpenGLTexture2D(const std::string& path);
 		virtual ~OpenGLTexture2D();
 
 		uint32_t GetWidth() const override { return m_Width; }
 		uint32_t GetHeight() const override { return m_Height; }
+
+		void SetData(void* data, uint32_t size) override;
 
 		void Bind(uint32_t slot = 0) const override;
 	};
