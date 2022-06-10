c9ef0e185beb8994bf6e61281d704a0b131a6861
FacelessTiger <thefacelesstiger@yahoo.com>
2022-06-10 09:23:27 -0700

    2D renderer transforms

--- Ellis/src/Ellis/Renderer/Renderer.cpp
+++ Ellis/src/Ellis/Renderer/Renderer.cpp
@@ -1,8 +1,6 @@
 #include "elpch.h"
 #include "Renderer.h"
 
-#include "Platform/OpenGL/OpenGLShader.h"
-
 #include "Ellis/Renderer/Renderer2D.h"
 
 namespace Ellis {
@@ -31,8 +29,8 @@ namespace Ellis {
 	void Renderer::Submit(const Ref<Shader>& shader, const Ref<VertexArray>& vertexArray, const glm::mat4& transform)
 	{
 		shader->Bind();
-		std::dynamic_pointer_cast<OpenGLShader>(shader)->UploadUniformMat4("u_ViewProjection", m_SceneData->ViewProjectionMatrix);
-		std::dynamic_pointer_cast<OpenGLShader>(shader)->UploadUniformMat4("u_Transform", transform);
+		shader->SetMat4("u_ViewProjection", m_SceneData->ViewProjectionMatrix);
+		shader->SetMat4("u_Transform", transform);
 
 		vertexArray->Bind();
 		RenderCommand::DrawIndexed(vertexArray);
