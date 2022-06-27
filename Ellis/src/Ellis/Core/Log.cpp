6b3846dc176bbf2b044f000ceadd220e8399bcf5
FacelessTiger <thefacelesstiger@yahoo.com>
2022-06-27 04:48:26 -0700

    Added some functionality to logging

--- Ellis/src/Ellis/Core/Log.cpp
+++ Ellis/src/Ellis/Core/Log.cpp
@@ -2,6 +2,7 @@
 #include "Log.h"
 
 #include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
 
 namespace Ellis {
 
@@ -11,13 +12,23 @@ namespace Ellis {
 	void Log::Init()
 	{
 		spdlog::set_level(spdlog::level::trace);
-		spdlog::set_pattern("%^[%T] %n: %v%$");
+		
+		std::vector<spdlog::sink_ptr> logSinks;
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Ellis.log", true));
 
-		s_CoreLogger = spdlog::stdout_color_mt("ELLIS");
-		s_CoreLogger->set_level(spdlog::level::trace);
+		logSinks[0]->set_pattern("%^[%T] %n: %v%$");
+		logSinks[1]->set_pattern("[%T] [%l] %n: %v");
 
-		s_ClientLogger = spdlog::stdout_color_mt("APP");
+		s_CoreLogger = std::make_shared<spdlog::logger>("ELLIS", begin(logSinks), end(logSinks));
+		spdlog::register_logger(s_CoreLogger);
 		s_CoreLogger->set_level(spdlog::level::trace);
+		s_CoreLogger->flush_on(spdlog::level::trace);
+
+		s_ClientLogger = std::make_shared<spdlog::logger>("APP", begin(logSinks), end(logSinks));
+		spdlog::register_logger(s_ClientLogger);
+		s_ClientLogger->set_level(spdlog::level::trace);
+		s_ClientLogger->flush_on(spdlog::level::trace);
 	}
 
 }
--- Ellis/src/Ellis/Core/Log.h
+++ Ellis/src/Ellis/Core/Log.h
@@ -2,8 +2,13 @@
 
 #include "Core.h"
 
+#define GLM_ENABLE_EXPERIMENTAL
+#include "glm/gtx/string_cast.hpp"
+
+#pragma warning(push, 0)
 #include <spdlog/spdlog.h>
 #include <spdlog/fmt/ostr.h>
+#pragma warning(pop)
 
 namespace Ellis {
 
@@ -19,6 +24,24 @@ namespace Ellis {
 		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
 	};
 
+}
+
+template<typename OStream, glm::length_t L, typename T, glm::qualifier Q>
+inline OStream& operator<<(OStream& os, const glm::vec<L, T, Q>& vector)
+{
+	return os << glm::to_string(vector);
+}
+
+template<typename OStream, glm::length_t C, glm::length_t R, typename T, glm::qualifier Q>
+inline OStream& operator<<(OStream& os, const glm::mat<C, R, T, Q>& matrix)
+{
+	return os << glm::to_string(matrix);
+}
+
+template<typename OStream, typename T, glm::qualifier Q>
+inline OStream& operator<<(OStream& os, glm::qua<T, Q> quaternion)
+{
+	return os << glm::to_string(quaternion);
 }
 
 // Core log macros
